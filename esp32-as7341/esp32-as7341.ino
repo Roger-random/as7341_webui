65f760beffefff55d6a068b9831a6f953b4778b3
Regorlas <roger.random@outlook.com>
2023-01-09 02:21:41 -0800

    Read AS7341 and return results as JSON

--- esp32-as7341/esp32-as7341.ino
+++ esp32-as7341/esp32-as7341.ino
@@ -4,14 +4,19 @@
 #include <ESPmDNS.h>
 #include "secrets.h"
 
+#include <Adafruit_AS7341.h>
+
 WebServer server(80);
+
+Adafruit_AS7341 as7341;
 
 const int led = 2;
 const char* mdns_name = "esp32-as7341";
+const char* usage = "Read AS7341 via endpopint /as7341/?atime=[0 through 255 inclusive]&astep=[0-65534]&gain=[0-9 as powers of 2]&ledma=[0, even numbers 4-150]";
 
 void handleRoot() {
   digitalWrite(led, 1);
-  server.send(200, "text/plain", "Read AS7341 via endpopint /as7341/?atime=[0 through 255 inclusive]&astep=[0-65534]&ledma=[0, 4-150]");
+  server.send(200, "text/plain", usage);
   digitalWrite(led, 0);
 }
 
@@ -19,6 +24,7 @@ void handleSensorRead() {
   int32_t atime=-1;
   int32_t astep=-1;
   int32_t ledma=-1;
+  int32_t gain=-1;
 
   digitalWrite(led, 1);
   for (uint8_t i = 0; i < server.args(); i++) {
@@ -28,29 +34,116 @@ void handleSensorRead() {
     if (0==server.argName(i).compareTo("astep")) {
       astep = server.arg(i).toInt();
     }
+    if (0==server.argName(i).compareTo("gain")) {
+      gain = server.arg(i).toInt();
+    }
     if (0==server.argName(i).compareTo("ledma")) {
       ledma = server.arg(i).toInt();
     }
   }
   if ((atime >= 0 && atime <= 255) &&
       (astep >= 0 && astep <= 65534) &&
-      (ledma == 0 || (ledma >= 4 && ledma <= 150))) {
-    String response = "{\n";
-    response += "  'settings' : {\n";
-    response += "    'atime' : ";
-    response += atime;
-    response += ",\n";
-    response += "    'astep' : ";
-    response += astep;
-    response += ",\n";
-    response += "    'ledma' : ";
-    response += ledma;
-    response += "\n";
-    response += "  }\n";
-    response += "}\n";
-    server.send(200, "application/json", response);
+      (gain  >= 0 && gain  <= 9) &&
+      (ledma == 0 || (ledma >= 4 && ledma <= 150 && (0 == ledma%2)))) {
+    uint16_t readings[12];
+
+    if (ledma > 0) {
+      as7341.enableLED(true);
+      as7341.setLEDCurrent(ledma);
+    } else {
+      as7341.enableLED(false);
+    }
+
+    as7341.setATIME(atime);
+    as7341.setASTEP(astep);
+    switch(gain) {
+      case 0:
+        as7341.setGain(AS7341_GAIN_1X);
+        break;
+      case 1:
+        as7341.setGain(AS7341_GAIN_2X);
+        break;
+      case 2:
+        as7341.setGain(AS7341_GAIN_4X);
+        break;
+      case 3:
+        as7341.setGain(AS7341_GAIN_8X);
+        break;
+      case 4:
+        as7341.setGain(AS7341_GAIN_16X);
+        break;
+      case 5:
+        as7341.setGain(AS7341_GAIN_32X);
+        break;
+      case 6:
+        as7341.setGain(AS7341_GAIN_64X);
+        break;
+      case 7:
+        as7341.setGain(AS7341_GAIN_128X);
+        break;
+      case 8:
+        as7341.setGain(AS7341_GAIN_256X);
+        break;
+      case 9:
+        as7341.setGain(AS7341_GAIN_512X);
+        break;
+    }
+
+    if (as7341.readAllChannels(readings)){
+      String response = "{\n";
+      response += "  \"415nm\" : ";
+      response += readings[AS7341_CHANNEL_415nm_F1];
+      response += ",\n";
+      response += "  \"445nm\" : ";
+      response += readings[AS7341_CHANNEL_445nm_F2];
+      response += ",\n";
+      response += "  \"480nm\" : ";
+      response += readings[AS7341_CHANNEL_480nm_F3];
+      response += ",\n";
+      response += "  \"515nm\" : ";
+      response += readings[AS7341_CHANNEL_515nm_F4];
+      response += ",\n";
+      response += "  \"555nm\" : ";
+      response += readings[AS7341_CHANNEL_555nm_F5];
+      response += ",\n";
+      response += "  \"590nm\" : ";
+      response += readings[AS7341_CHANNEL_590nm_F6];
+      response += ",\n";
+      response += "  \"630nm\" : ";
+      response += readings[AS7341_CHANNEL_630nm_F7];
+      response += ",\n";
+      response += "  \"680nm\" : ";
+      response += readings[AS7341_CHANNEL_680nm_F8];
+      response += ",\n";
+      response += "  \"clear\" : ";
+      response += readings[AS7341_CHANNEL_CLEAR];
+      response += ",\n";
+      response += "  \"nir\" : ";
+      response += readings[AS7341_CHANNEL_NIR];
+      response += ",\n";
+      response += "  \"settings\" : {\n";
+      response += "    \"atime\" : ";
+      response += atime;
+      response += ",\n";
+      response += "    \"astep\" : ";
+      response += astep;
+      response += ",\n";
+      response += "    \"gain\" : ";
+      response += (1 << gain);
+      response += ",\n";
+      response += "    \"ledma\" : ";
+      response += ledma;
+      response += "\n";
+      response += "  }\n";
+      response += "}\n";
+      server.send(200, "application/json", response);
+    } else {
+      server.send(500, "text/plain", "Failed readAllChannels()");
+    }
+
+    as7341.enableLED(false);
   } else {
-    server.send(400, "text/plain", "Read AS7341 via endpopint /as7341/?atime=[0 through 255 inclusive]&astep=[0-65534]&ledma=[0, 4-150]");
+    server.send(400, "text/plain", usage);
   }
 
   digitalWrite(led, 0);
@@ -77,9 +170,17 @@ void setup(void) {
   pinMode(led, OUTPUT);
   digitalWrite(led, 0);
   Serial.begin(115200);
+
+  // Start WiFi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
+
+  // Start AS7341
+  if (!as7341.begin()){
+    Serial.println("Could not find AS7341");
+    while (1) { delay(10); }
+  }
 
   // Wait for WiFi connection
   while (WiFi.status() != WL_CONNECTED) {
