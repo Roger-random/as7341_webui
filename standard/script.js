ca4d32b64c09cc5fdbbcf77bad6dcadea9d36586
Regorlas <roger.random@outlook.com>
2023-01-16 18:13:45 -0800

    Chart first draft

--- standard/script.js
+++ standard/script.js
@@ -5,6 +5,8 @@ var input_led;
 var input_go_button;
 var input_repeat_read;
 
+var spectral_chart;
+
 var value_atime;
 const value_astep = 3596; // (3596+1)*2.78us = 9.99966ms close enough to 10ms
 var value_gain;
@@ -15,6 +17,51 @@ var label_gain;
 var label_led;
 
 var raw_json;
+
+const spectral_labels = [
+  "415nm",
+  "445nm",
+  "480nm",
+  "515nm",
+  "555nm",
+  "590nm",
+  "630nm",
+  "680nm",
+  "nir"];
+
+// Conversion of F1-F8 wavelengths to RGB via
+// https://academo.org/demos/wavelength-to-colour-relationship/
+// https://www.johndcook.com/wavelength_to_RGB.html
+// NIR represented by an arbitrarily chosen dark red
+const spectral_colors = [
+  "#7600ED",
+  "#0028FF",
+  "#00D5FF",
+  "#1FFF00",
+  "#B3FF00",
+  "#FFDF00",
+  "#FF4F00",
+  "#FF0000",
+  "#220000"];
+
+const clear_label = "clear";
+var clear_data = 0;
+
+const devtest = true;
+
+function getURLobject() {
+  var espURI;
+
+  if (devtest) {
+    // Served from development machine, looking for ESP32 via mDNS name esp32-as7341.local
+    espURI = 'http://esp32-as7341.local/';
+  } else {
+    // When served from ESP32, hostname is our location.
+    espURI = window.location.href;
+  }
+
+  return new URL('/as7341',espURI);
+}
 
 function contentLoaded() {
   input_atime = document.getElementById('atime');
@@ -37,6 +84,34 @@ function contentLoaded() {
   raw_json = document.getElementById('raw_json');
 
   recalculate_parameters();
+
+  spectral_chart = new Chart(
+    document.getElementById('spectral_chart'),
+    {
+      type: 'bar',
+      data: {
+        labels: spectral_labels,
+        datasets: [{
+          data: [0,0,0,0,0,0,0,0,0],
+          borderColor: "#FFFFFF",
+          borderWidth: 3,
+          backgroundColor: spectral_colors
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        },
+        plugins: {
+          legend: {
+            display: false // Turn off legend
+          }
+        }, // Turn off legend
+        events: [] // Disable interactions (No click/hover/etc.)
+      }
+    });
 }
 
 function recalculate_parameters() {
@@ -60,7 +135,7 @@ function recalculate_parameters() {
 
 function initiate_read() {
   input_go_button.disabled = true;
-  var as7341 = new URL('http://esp32-as7341.local/as7341');
+  var as7341 = getURLobject();
   as7341.searchParams.set('atime', value_atime);
   as7341.searchParams.set('astep', value_astep);
   as7341.searchParams.set('gain', value_gain);
@@ -80,12 +155,17 @@ function display_raw_json(input_object) {
 }
 
 function process_sensor_data(sensor_data) {
+  var spectral_data = spectral_labels.map(x=>sensor_data[x]);
+  var clear_level = Number(sensor_data[clear_label]);
+  spectral_chart.data.datasets[0].data = spectral_data;
+  spectral_chart.options.scales.y.max = clear_level;
+  spectral_chart.update();
   display_raw_json(sensor_data);
   if(input_repeat_read.checked) {
     setTimeout(initiate_read);
   } else {
     // Turn off LED.
-    var as7341 = new URL('http://esp32-as7341.local/as7341');
+    var as7341 = getURLobject();
     as7341.searchParams.set('led_ma', 0);
     fetch(as7341); // Ignore response, we just wanted LED off.
 
