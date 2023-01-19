3a1e606700c754722df90e09791751f73f58c353
Regorlas <roger.random@outlook.com>
2023-01-18 16:47:21 -0800

    MIT license header

--- esp32-as7341/esp32-as7341.ino
+++ esp32-as7341/esp32-as7341.ino
@@ -1,3 +1,29 @@
+// MIT License
+
+// Copyright (c) 2023 Roger Cheng
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// AS7341 HTTP interface built by mashing up:
+// 1. ESP32 web server example 'HelloServer': https://github.com/espressif/arduino-esp32/tree/master/libraries/WebServer/examples/HelloServer
+// 2. Arduino AS7341 library example 'read_all_channels': https://github.com/adafruit/Adafruit_AS7341/blob/master/examples/read_all_channels
+
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
@@ -25,7 +51,7 @@ const char* usage = "AS7341 sensor HTTP GET endpoint\nRead spectral channels (op
 
 // Usually "false" so /as7341 endpoint is only accessible from pages we serve.
 // Can set to "true" to allow access from HTML UI from development machine.
-const bool allow_all_origin = true;
+const bool allow_all_origin = false;
 
 void handleBasicIndex() {
   digitalWrite(led, 1);
